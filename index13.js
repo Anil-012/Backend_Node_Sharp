b91f50138d0ab81704b6323a0a4cb7feb59b28f8
anil <anil.choudhary12195@gmail.com>
2023-09-24 18:43:35 +0530

    NodeMOn install

---
+++ index13.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+console.log("First node program");
+
+//using nodemon by installing npm install -g nodemon 
+
+// and it continously give output whenever we do any changes in code and also save 
+ 
