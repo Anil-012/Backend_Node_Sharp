b850e54a8e8ad1d33d8d45aeb94df3583936d5fb
anil <anil.choudhary12195@gmail.com>
2023-09-30 09:40:54 +0530

    Intro to Middlewares

---
+++ exp03.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// app.use('/', (req, res, next) => {
+//   console.log('This always runs!');
+//   next();
+// });
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res, next)=>{
+  // console.log('In another middleware!');
+  res.send('<form action = "/product" method ="POST"><input type="text" name = "title"><button type = "submit">Add product</button></form>');
+});
+app.use('/product', (req,res,next)=> {
+   console.log(req.body);
+   res.redirect('/');
+});
+
+app.use('/', (req, res, next)=> {
+  // console.log('in another middleware!');
+  res.send('<h1> Hello from Express!</h1>');
+});
+
+app.listen(3000);
